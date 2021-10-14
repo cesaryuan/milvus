0311b8f133209ac33b18cecc4b755d0a53f3c5ba
Ji Bin <matrixji@live.com>
2021-10-14 23:41:13 +0800

    Support for windows
    - Some adaptation for compile under msys/gcc-10.3
    - Deps under scripts/install_deps_msys.sh
      * mman-win32 for mmap syscall in c++
      * dummy libdl.a for -ld linking
    - adaptation for POSIX:
      * using gofrs/flock instead of syscall.Flock
      * using x/exp/mmap instead of syscall.Mmap
    - introducing github actions for build milvus.exe under windows/MSYS
    - using size_t/int64 instead of long in faiss, long under mingw always
      32bit long.
    
    Signed-off-by: Ji Bin <matrixji@live.com>

--- internal/core/src/index/thirdparty/faiss/impl/index_read.cpp
+++ internal/core/src/index/thirdparty/faiss/impl/index_read.cpp
@@ -70,9 +70,9 @@ namespace faiss {
 
 // will fail if we write 256G of data at once...
 #define READVECTOR(vec) {                       \
-        long size;                            \
+        size_t size;                            \
         READANDCHECK (&size, 1);                \
-        FAISS_THROW_IF_NOT (size >= 0 && size < (1L << 40));  \
+        FAISS_THROW_IF_NOT (size >= 0 && size < (1LL << 40));  \
         (vec).resize (size);                    \
         READANDCHECK ((vec).data (), size);     \
     }
