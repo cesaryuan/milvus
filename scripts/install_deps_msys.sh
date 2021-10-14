ed85986f1992d34567a44b3677c903be1cd1120e
Ji Bin <matrixji@live.com>
2021-10-14 23:41:13 +0800

    Support for windows
    - Some adaptation for compile under msys/gcc-10.3
    - Deps under scripts/install_deps_msys.sh
      * mman-win32 for mmap syscall in c++
      * dummy libdl.a for -ld linking
    - adaptation for POSIX:
      * using gofrs/flock instead of syscall.Flock
      * using x/exp/mmap instead of syscall.Mmap
    - introducing github actions for build milvus.exe under windows/MSYS
    - using size_t/int64 instead of long in faiss, long under mingw always
      32bit long.
    
    Signed-off-by: Ji Bin <matrixji@live.com>

---
+++ scripts/install_deps_msys.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -e
+
+if [[ "${MSYSTEM}" != "MINGW64" ]] ; then
+    echo non MINGW64, exit.
+    exit 1
+fi
+
+pacman -Su --noconfirm --needed \
+    git make tar dos2unix zip unzip \
+    mingw-w64-x86_64-toolchain \
+    mingw-w64-x86_64-make \
+    mingw-w64-x86_64-cmake \
+    mingw-w64-x86_64-go \
+    mingw-w64-x86_64-arrow \
+    mingw-w64-x86_64-boost \
+    mingw-w64-x86_64-rocksdb=6.12.7 \
+    mingw-w64-x86_64-intel-tbb \
+    mingw-w64-x86_64-openblas
+
+rm -fr mman-win32 && \
+    git clone https://github.com/alitrack/mman-win32.git mman-win32 && \
+    cd mman-win32 && \
+    cmake -G "MSYS Makefiles" \
+          -DCMAKE_MAKE_PROGRAM=mingw32-make \
+          -DCMAKE_INSTALL_PREFIX=/mingw64 \
+          -DBUILD_SHARED_LIBS=OFF . && \
+    mingw32-make install
+
+cd - && rm -fr mman-win32
+
+touch a.c && \
+    gcc -c a.c && \
+    ar rc libdl.a a.o && \
+    ranlib libdl.a && \
+    cp -fr libdl.a /mingw64/lib && \
+    rm -fr a.c a.o libdl.a
+
